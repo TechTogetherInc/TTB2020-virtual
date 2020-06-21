d352ed1369f3c20d8e420a58ef736bf75b75eec7
Mitali Salvi <salvi.mi@northeastern.edu>
2020-06-20 23:36:37 -0400

    Adding sponsor logos

--- src/components/sponsors/sponsor.js
+++ src/components/sponsors/sponsor.js
@@ -1,9 +1,155 @@
 import React from "react";
+
+import bu from "../../images/partner-logos/BU-logo.png";
+import wayfair from "../../images/partner-logos/Wayfair.png";
+import redhat from "../../images/partner-logos/redHet_Logo.png";
+import aws from "../../images/partner-logos/AWS_Services.png";
+import dell from "../../images/partner-logos/dell-blue-logo.png";
+import slalom from "../../images/partner-logos/Slalom_Build.png";
+import atlassian from "../../images/partner-logos/Atlassian-horizontal.png";
+import twosigma from "../../images/partner-logos/twosigmalogo.png";
+import bcbs from "../../images/partner-logos/BCBS.png";
+import mathworks from "../../images/partner-logos/MathWorks.jpg";
+import capitalone from "../../images/partner-logos/capitalone-logo.png";
+import facebook from "../../images/partner-logos/facebook-logo-gray-v2.png";
+import ibm from "../../images/partner-logos/IBM_research_logo.png";
+import cbre from "../../images/partner-logos/CBRE_logo.png";
+import qualcomm from "../../images/partner-logos/Qualcomm-Logo.jpg";
+import bloomberg from "../../images/partner-logos/BBGblck.png";
+import liberty from "../../images/partner-logos/Liberty_Mutual.png";
+import northeastern from "../../images/partner-logos/northeastern.png";
+import balsamiq from "../../images/partner-logos/balsamiq-logo-noborder-print.png";
+import gc from "../../images/partner-logos/gc.png";
+import gwc from "../../images/partner-logos/gwc.png";
+import armoured from "../../images/partner-logos/armored-things-logo.png";
+import vistaprint from "../../images/partner-logos/vistaprint-logo.png";
+import samsonite from "../../images/partner-logos/Samsonitelogo_PMS280.jpg";
+import spark from "../../images/partner-logos/spark.png";
+import bit from "../../images/partner-logos/theBit.png";
+import stickermule from "../../images/partner-logos/sticker-mule-logo.png";
+import stringloop from "../../images/partner-logos/stringandloop_Logo_Black.png";
+
+
 const Sponsor = () => (
   <div>
-    <h1>Our sponsors this year</h1>
+    <h1>Our past sponsors</h1>
     <div className="bar-sm"></div>
-  </div>
+    <div className="sponsor-gallery">
+
+      <div class="gallery">
+        <img src={bu} alt="Boston University"  />
+      </div>
+
+      <div class="gallery">
+        <img src={wayfair} alt="Wayfair"  />
+      </div>
+
+      <div class="gallery">
+        <img src={redhat} alt="RedHat"  />
+      </div>
+
+      <div class="gallery">
+        <img src={aws} alt="AWS"  />
+      </div>
+
+      <div class="gallery">
+        <img src={dell} alt="Dell"  />
+      </div>
+
+      <div class="gallery">
+        <img src={slalom} alt="Slalom"  />
+      </div>
+
+      <div class="gallery">
+        <img src={atlassian} alt="Atlassian"  />
+      </div>
+
+      <div class="gallery">
+        <img src={twosigma} alt="Two-Sigma"  />
+      </div>
+
+      <div class="gallery">
+        <img src={bcbs} alt="BCBS"  />
+      </div>
+
+      <div class="gallery">
+        <img src={mathworks} alt="Mathworks"  />
+      </div>
+
+      <div class="gallery">
+        <img src={capitalone} alt="Capital-One"  />
+      </div>
+
+      <div class="gallery">
+        <img src={facebook} alt="Facebook"  />
+      </div>
+
+      <div class="gallery">
+        <img src={ibm} alt="IBM-Research"  />
+      </div>
+
+      <div class="gallery">
+        <img src={cbre} alt="CBRE"  />
+      </div>
+
+      <div class="gallery">
+        <img src={qualcomm} alt="Qualcomm"  />
+      </div>
+
+      <div class="gallery">
+        <img src={bloomberg} alt="Bloomberg"  />
+      </div>
+
+      <div class="gallery">
+        <img src={liberty} alt="Liberty Mutual"  />
+      </div>
+
+      <div class="gallery">
+        <img src={northeastern} alt="Northeastern University"  />
+      </div>
+
+      <div class="gallery">
+        <img src={balsamiq} alt="Balsamiq"  />
+      </div>
+
+      <div class="gallery">
+        <img src={gc} alt="General Catalyst"  />
+      </div>
+
+      <div class="gallery">
+        <img src={gwc} alt="Girls Who Code"  />
+      </div>
+
+      <div class="gallery">
+        <img src={armoured} alt="Armoured Things"  />
+      </div>
+
+      <div class="gallery">
+        <img src={vistaprint} alt="Vistaprint"  />
+      </div>
+
+      <div class="gallery">
+        <img src={samsonite} alt="Samsonite"  />
+      </div>
+
+      <div class="gallery">
+        <img src={spark} alt="Spark"  />
+      </div>
+
+      <div class="gallery">
+        <img src={bit} alt="The Bit"  />
+      </div>
+
+      <div class="gallery">
+        <img src={stickermule} alt="Sticker Mule"  />
+      </div>
+
+      <div class="gallery">
+        <img src={stringloop} alt="String Loop"  />
+      </div>
+
+      </div>
+    </div>
 );
 
 export default Sponsor;
