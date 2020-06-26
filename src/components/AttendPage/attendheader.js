45a203db1a34b1d9b2ff6a15b502391e991e0a67
Hannah Huang <yunhanh@bu.edu>
2020-06-26 14:44:41 -0400

    add attend page

--- src/components/AttendPage/attendheader.js
+++ src/components/AttendPage/attendheader.js
@@ -3,6 +3,8 @@ import React from "react";
 const AttendHeader = () => (
   <div className="attend">
     <h1>Coming Soon</h1>
+    {/* // HERE: Subscribe us to get notified when the applciation is open
+     */}
   </div>
 );
 export default AttendHeader;
