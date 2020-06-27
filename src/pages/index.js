a0d2441408a743286377d7229cbbf6f08316eb56
Hannah Huang <yunhanh@bu.edu>
2020-06-26 20:21:10 -0400

    update info

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ const App = () => {
                   Boston's largest all-female, femme, <br />
                   and non-binary hackathon.
                 </h3>
-                <h3>November 6 - November 7, 2020</h3>
+                <h3>November 6 - November 8, 2020</h3>
                 <div
                   className="button-container"
                   style={{ justifyContent: "center" }}
