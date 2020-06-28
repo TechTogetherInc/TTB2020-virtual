4246e142c1d4cde71083ee96e9831e32cd1a3148
Hannah Huang <yunhanh@bu.edu>
2020-06-27 20:45:02 -0400

    add statistics

--- src/components/Statistics/statistics.js
+++ src/components/Statistics/statistics.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "../../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+import photo1 from "../../images/stats1.png";
+import photo2 from "../../images/stats2.png";
+import photo3 from "../../images/stats3.png";
+import photo4 from "../../images/stats4.png";
+import photo5 from "../../images/stats5.png";
 const Statistics = () => (
   <div>
     <h1>2020 TTB Statistics</h1>
-    <div className="bar-sm"></div>
+    <div className=" carousel">
+      <Carousel>
+        <div>
+          <img src={photo1} />
+        </div>
+        <div>
+          <img src={photo2} />
+        </div>
+        <div>
+          <img src={photo3} />
+        </div>
+        <div>
+          <img src={photo4} />
+        </div>
+        <div>
+          <img src={photo5} />
+        </div>
+      </Carousel>
+    </div>
   </div>
 );
 export default Statistics;
