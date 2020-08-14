b6d918cc1e5d8003a6ffd50d86c17e5b5358e103
Hannah Huang <yunhanh@bu.edu>
2020-08-14 18:38:39 -0400

    add new page

---
+++ src/pages/learnMore.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <LearnMore />
+  </Layout>
+);
+
+const LearnMore = () => {
+  return <div>Hello</div>;
+};
