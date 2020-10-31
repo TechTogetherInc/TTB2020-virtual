0f44107a2af6c7a8615a948506e110f6d1dd5fea
Mitali Salvi <salvi.mi@northeastern.edu>
2020-10-30 21:17:22 -0400

    Updated speaker section to show tabs

--- src/components/speaker-mentor/speaker-tile.js
+++ src/components/speaker-mentor/speaker-tile.js
@@ -1,18 +1,44 @@
 import React from "react";
 import SpeakersData from "../../data/speakers.json";
 
-import Aurora from "../../images/aurora.png";
+import Richard from "../../images/speakers/richard.png";
+import Carolyn from "../../images/speakers/carolyn.png";
+import Savita from "../../images/speakers/savita.png";
+import Charis from "../../images/speakers/charis.png";
+import Liat from "../../images/speakers/liat.png";
+
+const photos = {
+    Richard,
+    Carolyn,
+    Savita,
+    Charis,
+    Liat,
+};
+
+// const Speaker = (props) => (
+//     <div class="card card-width">
+//         <img class="card-img-top img-fluid" src={photos[props.image]} alt={props.image}/>
+//         <div class="card-body">
+//             <h5 class="card-title">{props.name}</h5>
+//             <h6>{props.company}</h6>
+//             <h6>{props.title}</h6>
+//         </div>
+//         <div class="card-body">
+//             <p class="card-text">{props.aboutMe}</p>
+//             <a href={props.linkedinUrl} target="_blank" class="card-link">Contact Me</a>
+//         </div>
+//     </div>
+// );
 
 const Speaker = (props) => (
-    <div class="card card-width">
-        <img class="card-img-top" src={Aurora} alt="Card image cap" />
-        <div class="card-body">
-            <h5 class="card-title">Speaker Name</h5>
-            <h6>Speaker Title</h6>
+    <div class="row mb-2">
+        <div className="col-12 col-md-3">
+            <img src={photos[props.image]} alt="oops" className="speaker-img" />
         </div>
-        <div class="card-body">
-        <p class="card-text">Something about me. Something about me. Something about me. Something about me. Something about me.</p>
-            <a href="#" class="card-link">Bitly link</a>
+        <div className="col-12 col-md-9 speaker-text">
+            <h2 className="speaker-name">{props.name}</h2>
+            <h5 className="speaker-title">{props.title} @ {props.company}</h5>
+            <p className="speaker-description">{props.aboutMe}</p>
         </div>
     </div>
 );
@@ -22,12 +48,14 @@ const SpeakerTiles = () => (
         <h1 className="speaker-head">TTB 2020 Speakers</h1>
         <div className="speaker-container row">
             {SpeakersData.map((speaker) => (
-                <div className="speaker col-sm" key={speaker.name}>
+                <div className="speaker" key={speaker.name}>
                     <Speaker
-                        name={speaker.title}
-                        link={speaker.link}
+                        name={speaker.name}
+                        company={speaker.company}
+                        title={speaker.title}
+                        linkedinUrl={speaker.linkedinUrl}
                         image={speaker.image}
-                        description={speaker.description}
+                        aboutMe={speaker.aboutMe}
                     />
                 </div>
             ))}
