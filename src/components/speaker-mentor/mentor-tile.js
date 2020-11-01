11e501479598ce7b293489df134c1aab9c95b86c
Mitali Salvi <salvi.mi@northeastern.edu>
2020-11-01 17:16:11 -0500

    Modifying mentor page

--- src/components/speaker-mentor/mentor-tile.js
+++ src/components/speaker-mentor/mentor-tile.js
@@ -3,14 +3,16 @@ import React from "react";
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
-import MentorsData from "../../data/mentors.json";
+import Image from "./image"
 
-import Richard from "../../images/speakers/richard.png";
+import MentorsData from "../../data/mentors.json";
 
 const Mentor = (props) => (
     <div class="card card-width">
         <div className="card-img-div">
-            <img class="card-img-top mentor-img" src={Richard} alt="Card image cap" />
+            <div className="card-img-top" >
+                <Image alt="headshot" filename={props.photo} />
+            </div>
         </div>
         <div class="card-body">
             <h5 class="card-title">{props.name} ({props.pronouns})</h5>
@@ -36,7 +38,23 @@ const Mentor = (props) => (
 
 const MentorTiles = () => (
     <div>
-        <h1 className="mentor-head">TTB 2020 mentors</h1>
+        <div class="row">
+            <div class="col-sm-6">
+                <h1 className="mentor-head">TTB 2020 mentors</h1>
+                <p className="mentor-head">To view who's online, check out our Discord !</p>
+            </div>
+            <div class="col-sm-6" className="contact-mentor">
+                <div className="register-button">
+                    <a
+                        type="button"
+                        target="_blank"
+                    // href="https://techtogetherboston.eventbrite.com/"
+                    >
+                        Contact A Mentor
+                    </a>
+                </div>
+            </div>
+        </div>
         <div className="mentor-container row">
             {MentorsData.map((mentor) => (
                 <div className="mentor" key={mentor.name}>
