5d9e1893f3b1139806dcc6345b08dabdf1936828
liua13 <liua1@bxscience.edu>
2020-11-04 19:39:09 -0500

    modal for sponsor blurbs

--- src/components/sponsors/actualSponsor.js
+++ src/components/sponsors/actualSponsor.js
@@ -1,37 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import SponsorData from "../../data/sponsors.json";
+import ReactModal from "react-modal";
 // import { StaticQuery, graphql } from "gatsby";
 import Image from "./image";
 
+ReactModal.setAppElement("#___gatsby");
+
 const ActualSponsor = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sponsorName, setSponsorName] = useState("");
+  const [sponsorBlurb, setSponsorBlurb] = useState("");
+
+  const openModal = (name, blurb) => {
+    if (blurb) {
+      setSponsorName(name);
+      setSponsorBlurb(blurb);
+      setIsModalOpen(true);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
-      <h1>Our Current Sponsors</h1>
+      <h1 style={{ marginBottom: "2px" }}>Our Current Sponsors</h1>
+      <p style={{ fontStyle: "italic" }}>
+        Click on the sponsor logos for more information!
+      </p>
+      <ReactModal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Sponsor Blurbs"
+        className="Modal"
+        overlayClassName="Overlay"
+        scrollable={true}
+      >
+        <h1 style={{ color: "black", textAlign: "right" }} onClick={closeModal}>
+          x
+        </h1>
+        <h1 style={{ color: "black", marginBottom: "25px" }}>{sponsorName}</h1>
+        <p dangerouslySetInnerHTML={{ __html: sponsorBlurb }}></p>
+      </ReactModal>
+
+      {/* sponsors */}
       <div className="row">
         <div style={{ width: "325px", margin: "auto" }}>
-          <Image alt="headshot" filename={"sponsor-logos/amazon.png"} />
+          <Image filename={"sponsor-logos/amazon.png"} />
         </div>
         <div style={{ width: "325px", margin: "auto" }}>
-          <Image alt="headshot" filename={"sponsor-logos/BU-logo.png"} />
+          <Image filename={"sponsor-logos/BU-logo.png"} />
         </div>
       </div>
       <div className="row">
-        <div style={{ width: "225px", margin: "auto" }}>
-          <Image
-            alt="headshot"
-            filename={"sponsor-logos/pegasystem-logo.jpg"}
-          />
+        <div
+          style={{ width: "225px", margin: "auto" }}
+          onClick={() =>
+            openModal(
+              "Pegasystem",
+              "Pega is the leader in cloud software for customer engagement and operational excellence. The world’s most recognized and successful brands rely on Pega’s AI-powered software to optimize every customer interaction on any channel while ensuring their brand promises are kept. Pega’s low-code application development platform allows enterprises to quickly build and evolve apps to meet their customer and employee needs and drive digital transformation on a global scale."
+            )
+          }
+        >
+          <Image filename={"sponsor-logos/pegasystem-logo.jpg"} />
         </div>
 
         <div style={{ width: "225px", margin: "auto" }}>
-          <Image
-            alt="headshot"
-            filename={"sponsor-logos/capitalone-logo.png"}
-          />
+          <Image filename={"sponsor-logos/capitalone-logo.png"} />
         </div>
 
-        <div style={{ width: "225px", margin: "auto" }}>
-          <Image alt="headshot" filename={"sponsor-logos/redhat-logo.png"} />
+        <div
+          style={{ width: "225px", margin: "auto" }}
+          onClick={() =>
+            openModal(
+              "Red Hat",
+              "Red Hat is the world’s leading provider of enterprise open source solutions, using a community-powered approach to deliver high-performing Linux, cloud, container, and Kubernetes technologies. Red Hat helps you standardize across environments, develop cloud-native applications, and integrate, automate, secure, and manage complex environments with award-winning support, training, and consulting services."
+            )
+          }
+        >
+          <Image filename={"sponsor-logos/redhat-logo.png"} />
         </div>
       </div>
       <div className="row">
@@ -39,8 +87,9 @@ const ActualSponsor = () => {
           <div
             className=""
             style={{ width: "150px", margin: "auto", padding: "5px" }}
+            onClick={() => openModal(sponsor.sponsor, sponsor.blurb)}
           >
-            <Image alt="headshot" filename={sponsor.logo} />
+            <Image filename={sponsor.logo} />
           </div>
         ))}
       </div>
